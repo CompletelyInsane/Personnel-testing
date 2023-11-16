baa6ef068226d6cc803ecd3ebeaa7dbcf3fd0295
LoveToEatOrangePeel <2834264856@qq.com>
2023-11-16 17:51:34 +0800

    修改了参数

--- Personnel-testing/Personnel-testing/com.h
+++ Personnel-testing/Personnel-testing/com.h
@@ -13,8 +13,8 @@ uint8_t   BeLast_State = 0x00,   last_State = 0x00,  Current_State = 0x00  , Sta
 int  TIM_refer = 0 , CoverCou = 0, CoverSumIN = 0, CoverSumOut = 0,  Radarinit = 0 , Radarinit1 = 0 ,ReferenceNum = 200;
 uint8_t Errornum = 0 , Errornum1 = 0; //连续错误数量
 bool ErrorFlag = 0;
-#define  high  0.9    //雷达值高于无遮挡状态的90% 算无遮挡
-#define  Low   0.7    //雷达值低于无遮挡状态的70% 算有遮挡
+#define  high  0.95    //雷达值高于无遮挡状态的90% 算无遮挡
+#define  Low   0.85    //雷达值低于无遮挡状态的70% 算有遮挡
 
 //雷达数据结构体
  struct TF{
